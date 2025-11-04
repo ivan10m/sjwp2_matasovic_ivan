421e213d62c4424b8b429ef495729fc8055c82f9
ivan10m <ivan.matasovic1@gmail.com>
2025-11-04 15:25:36 +0100

    dice game

--- Dicee_game/index.js
+++ Dicee_game/index.js
@@ -1,3 +1,4 @@
+
 let randomNumber1 = Math.floor(Math.random()*6)+1;
 let randomImageSource1 = "./images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
